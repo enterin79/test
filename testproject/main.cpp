598ba518b45d2a41a31b04c2638cc9102b1fdb71
enterin79 <nastuazh@gmail.com>
2021-02-01 16:07:49 +0300

    First commit

---
+++ testproject/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QLabel lbl("Hello world");
+    lbl.show();
+    return a.exec();
+}
