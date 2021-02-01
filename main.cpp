fc8e8fee61f8e5d15faafe4ce2662a3e555a3f8d
enterin79 <nastuazh@gmail.com>
2021-02-01 16:45:49 +0300

    олололо

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QLabel lbl("Hello world");
+    lbl.show();
+    return a.exec();
+    //олололол
+}
